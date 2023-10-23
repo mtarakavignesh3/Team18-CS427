28f77de8ed60675da008f0b10efdc426a623a81d
David Valencia Sanchez <51679082+svalenciadavid@users.noreply.github.com>
2023-10-23 00:06:11 -0500

    fixes for login

--- app/src/main/java/edu/uiuc/cs427app/LoginActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/LoginActivity.java
@@ -1,36 +1,72 @@
 package edu.uiuc.cs427app;
 
+import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AccountManager accountManager;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button register_button = findViewById(R.id.create_an_account_button);
-        register_button.setOnClickListener(new View.OnClickListener() {
+        accountManager = AccountManager.get(this);
+        usernameEditText = findViewById(R.id.login_username);
+        passwordEditText = findViewById(R.id.login_password);
+
+        Button loginBtn = findViewById(R.id.login_button);
+        loginBtn.setOnClickListener(this);
+
+        Button createAccountBtn = findViewById(R.id.create_an_account_button);
+
+        createAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 registerActivity();
             }
         });
     }
+
     private void registerActivity() {
-        Intent intent;
-        intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
     @Override
     public void onClick(View view) {
-        Intent intent;
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Missing Fields: Please type a username and a password", Toast.LENGTH_SHORT).show();
+        } else {
+            // Authenticate the user
+            Account[] accounts = accountManager.getAccountsByType("edu.uiuc.cs427app");
+            for (Account account : accounts) {
+                if (account.name.equals(username)) {
+                    String accountPassword = accountManager.getPassword(account);
+                    if (password.equals(accountPassword)) {
+
+                        Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                        // TODO: NAVIGATE TO MAIN PAGE:
+
+                        return;
+                    }
+                }
+            }
+            // Authentication failed
+            Toast.makeText(LoginActivity.this, "Login failed. Please check your credentials.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/edu/uiuc/cs427app/RegisterActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/RegisterActivity.java
@@ -3,6 +3,7 @@ package edu.uiuc.cs427app;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.accounts.AccountManager;
 import android.accounts.Account;
@@ -13,28 +14,40 @@ import androidx.appcompat.app.AppCompatActivity;
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AccountManager accountManager;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         accountManager = AccountManager.get(this);
+        usernameEditText = findViewById(R.id.register_username);
+        passwordEditText = findViewById(R.id.register_password);
+
         Account[] accounts = accountManager.getAccountsByType("edu.uiuc.cs427app");
 
-        Button register = findViewById(R.id.register_button);
-        register.setOnClickListener(this);
+        Button registerButton = findViewById(R.id.register_button);
+        registerButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-//        TextView username = findViewById(R.id.register_username);
-//        TextView password = findViewById(R.id.register_password);
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
 
-        Account newAccount = new Account("username", "edu.uiuc.cs427app");
-//        accountManager.addAccountExplicitly(newAccount, "password", null);
-//        accountManager.setAuthToken(newAccount, "test_accounts", "auth_token");
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Missing Fields: Please type a username and a password", Toast.LENGTH_SHORT).show();
+        } else {
+            Account newAccount = new Account(username, "edu.uiuc.cs427app");
+            boolean accountAdded = accountManager.addAccountExplicitly(newAccount, password, null);
 
-        Toast.makeText(RegisterActivity.this,
-                "Account Registered", Toast.LENGTH_LONG).show();
+            if (accountAdded) {
+                Toast.makeText(this, "Account Registered", Toast.LENGTH_SHORT).show();
+                finish(); // Close the registration activity
+            } else {
+                Toast.makeText(this, "Failed to register the account", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
